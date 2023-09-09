73d2d130e7fa593b4d9d667a6724dba135337ad4
Jonathan Ramirez <jonathanramireznunez23@gmail.com>
2023-09-09 10:02:14 -0700

    added Todos logic and separated app into components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,51 @@
+import { useState } from "react"
 import "./styles.css"
+import { NewTodoForm } from "./NewTodoForm"
+import { TodoList } from "./TodoList"
 
 export default function App() {
+  
+  const [todos, setTodos] = useState([])
+
+  function addTodo(title) {
+    // use a function like this to get the most recent value of todos.
+    // if you just use "todos", it will get the original immutable "todos"
+    // value, which in this case is []
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos, 
+        { id: crypto.randomUUID(), title, completed: false },
+      ]
+    })
+  }
+
+  // returns new todo object, used when onClick event occurs on checkbox
+  function toggleTodo(id, completed){
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          // The state is immutable so you can't do:
+          // todo.completed = completed
+          // So we have to create a new todo object with 'completed' changed
+          return { ...todo, completed }
+        }
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id )
+    })
+  }
+
   return (
-    <form className="new-item-form">
-      <div className="form-row">
-        <label htmlFor="item">New Item</label>
-        <input type="text" id="item" />
-      </div>
-      <button className="btn">Add</button>
-    </form>
+    <>
+      {/* passing addTodo function to addTodo prop */}
+      <NewTodoForm onSubmit={addTodo} />
+      <h1 className="header">Todo List</h1>
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+    </>
   )
 }
---
+++ src/TodoList.jsx
@@ -0,0 +1,22 @@
+import { TodoItem } from "./TodoItem";
+
+export function TodoList({ todos, toggleTodo, deleteTodo }) {
+  return (
+    <ul className="list">
+      {/*React short circuiting returns if true, will use as placeholder*/}
+      {todos.length === 0 && "No Todos"}
+      {todos.map((todo) => {
+        return (
+          // React requires a key to know which todo to change if a
+          // change is requested, otherwise console errors appear.
+          <TodoItem
+            {...todo}
+            key={todo.id}
+            toggleTodo={toggleTodo}
+            deleteTodo={deleteTodo}
+          />
+        );
+      })}
+    </ul>
+  );
+}
